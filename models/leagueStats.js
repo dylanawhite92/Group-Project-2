64926794705f00e19bfef5ac778cd500c5b415d3
pholmes10 <pholmes10@gmail.com>
2019-01-04 11:52:39 -0600

    added model for rest of league

---
+++ models/leagueStats.js
@@ -0,0 +1,33 @@
+module.exports = function (sequelize, DataTypes) {
+    var League = sequelize.define("League", {
+        team_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        player_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        player_position: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        player_salary: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        ppg: {
+            type: DataTypes.INTEGER,
+        },
+    });
+    return League;
+};
