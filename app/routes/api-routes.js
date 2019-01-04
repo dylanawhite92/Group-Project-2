0a06275911a610d5dfd5837d75fe9974f0df960e
Dylan White <dylanawhite11@gmail.com>
2019-01-04 12:46:30 -0600

    testing server routing

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local", { failureRedirect: "/" }), function(req, res) {
-      res.redirect("/home");
+      res.json("/home");
   });
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
